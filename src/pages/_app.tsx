25ee56f99d020e1f209e7e7b085176b418c37d97
bonabrian <bonabriansiagian@gmail.com>
2022-12-13 18:12:17 +0700

    feat: add chakra-ui packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 import { GlobalStyles } from '@/styles'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ChakraProvider>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   )
 }
 
